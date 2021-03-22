eaefb351d299ccb017197a054dac4b49d6d8f59c
Abdelilah <lbardi.abdelilah@gmail.com>
2021-03-22 15:59:22 +0100

    Adds SPA route

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/mails', \App\Http\Controllers\Mails\ListMailsController::class)->name('mails.index');
 Route::get('/mails/{mail}', \App\Http\Controllers\Mails\ShowMailController::class)->name('mails.show');
 Route::post('/mails', \App\Http\Controllers\Mails\SendMailController::class)->name('mails.create');
+
+
+Route::view('/{any}', 'welcome')->where('any', '.*');
