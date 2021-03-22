bbec4dfe0e65d0de4d7dce0d9e2c5fd06ac881c1
Abdelilah <lbardi.abdelilah@gmail.com>
2021-03-22 02:47:15 +0100

    Adds mail status updaters and orders mail statuses by newest to oldest

--- app/Models/Mail.php
+++ app/Models/Mail.php
@@ -26,7 +26,7 @@ class Mail extends Model
      */
     public function statuses(): HasMany
     {
-        return $this->hasMany(Status::class);
+        return $this->hasMany(Status::class)->orderByDesc('id');
     }
 
     /**
@@ -35,5 +35,35 @@ class Mail extends Model
     public function status(): HasOne
     {
         return $this->hasOne(Status::class)->orderByDesc('id');
+    }
+
+    /**
+     * @return $this
+     */
+    public function markAsPosted(): Mail
+    {
+        $this->statuses()->create([ 'status' => Status::POSTED ]);
+
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    public function markAsSent(): Mail
+    {
+        $this->statuses()->create([ 'status' => Status::SENT ]);
+
+        return $this;
+    }
+
+    /**
+     * @return $this
+     */
+    public function markAsFailed(): Mail
+    {
+        $this->statuses()->create([ 'status' => Status::FAILED ]);
+
+        return $this;
     }
 }
