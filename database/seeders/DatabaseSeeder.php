10b687a9379772406a2b1ee383bc959eb9c78135
Abdelilah <lbardi.abdelilah@gmail.com>
2021-03-22 12:56:19 +0100

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Mail;
+use App\Models\Status;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Mail::factory()->count(100)->create()->each(function (Mail $mail) {
+
+            $randomStatus = array_rand([
+                Status::POSTED => '1',
+                Status::SENT => '2',
+                Status::FAILED => '3',
+            ]);
+
+            if ($randomStatus == Status::SENT) {
+                $mail->update([
+                    'sent_at' => now()
+                ]);
+            }
+
+            $mail->statuses()->create([
+                'status' =>  $randomStatus
+            ]);
+
+        });
     }
 }
